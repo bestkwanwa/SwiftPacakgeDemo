78bd07fb015be2832e4eac66be2187b766513b7e
Elvis Yang <v176juy@cathaypacific.com>
2023-02-22 14:53:33 +0800

    feat: test fn

--- Sources/SwiftPacakgeDemo/SwiftPacakgeDemo.swift
+++ Sources/SwiftPacakgeDemo/SwiftPacakgeDemo.swift
@@ -2,5 +2,11 @@ public struct SwiftPacakgeDemo {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print(text)
+    }
+}
+public class Test{
+    public func fn(){
+        print("123")
     }
 }
